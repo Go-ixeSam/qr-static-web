eb1303a8d975e30f666aac284542ed61a7df35e3
sam nguyen <42427441+Go-ixeSam@users.noreply.github.com>
2021-04-21 16:27:48 +0700

    Cập nhật lấy exchange ID từ param của address URL

--- src/App.js
+++ src/App.js
@@ -12,35 +12,40 @@ function App() {
     place_id: "",
     formatted_address: "",
   });
+  const [Id, setID] = useState("");
+  let [alrigh, setAlrigh] = useState(true);
+  const Error = (
+    <p style={{ color: "red" }}>Hệ thống bị lỗi, hãy thử lại lần nữa</p>
+  );
   React.useEffect(() => {
-    APICalling.getQRCodeInfo("77986141-cc78-498f-b8b4-e4de5ecf3b37").then(
-      (res) => {
-        const data = res.data;
-        setExchangeData({
-          giver: data.fullNameHost,
-          receiver: data.fullNameReceiver,
-          phone: "",
-          receiverAddress: data.receiverAddress,
-          quantity: data.quantity,
-          vegetable: data.vegNameReceive,
-          qrcode: data.qrCode,
-        });
-        APICalling.covertAddressToCondinate(data.receiverAddress).then(
-          (res) => {
-            const resutl = res.data.results[0];
-            const tmp = {
-              ...address,
-              lat: resutl.formatted_address,
-              place_id: resutl.place_id,
-              long: resutl.geometry.location.lng,
-              formatted_address: resutl.formatted_address,
-              lat: resutl.geometry.location.lat,
-            };
-            setAddress(tmp);
-          }
-        );
-      }
-    );
+    // console.log("link",document.location.href,"và search, ",document.location.search)
+    const params = new URLSearchParams(document.location.search); //* lấy đoạn query tring trong URL
+    const ID = params.get("Id"); // * lấy ra giá trị của param đầu vào
+    console.log(document.location.search);
+    APICalling.getQRCodeInfo(ID).then((res) => {
+      const data = res.data;
+      setExchangeData({
+        giver: data.fullNameHost,
+        receiver: data.fullNameReceiver,
+        phone: "",
+        receiverAddress: data.receiverAddress,
+        quantity: data.quantity,
+        vegetable: data.vegNameReceive,
+        qrcode: data.qrCode,
+      });
+      APICalling.covertAddressToCondinate(data.receiverAddress).then((res) => {
+        const resutl = res.data.results[0];
+        const tmp = {
+          ...address,
+          lat: resutl.formatted_address,
+          place_id: resutl.place_id,
+          long: resutl.geometry.location.lng,
+          formatted_address: resutl.formatted_address,
+          lat: resutl.geometry.location.lat,
+        };
+        setAddress(tmp);
+      });
+    });
   }, []);
   const size = 20;
   const qrcodeSize = 200;
@@ -56,42 +61,44 @@ function App() {
     vegetable,
     qrcode,
   } = exchangeData;
-  return (
-    <React.Fragment>
-      <img src={qrcode} height={qrcodeSize} width={qrcodeSize} />
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          marginLeft: 10,
-          width: "max-content",
-        }}
-      >
-        <div className="styling">
-          <p>Người cho </p>
-          <p>Người nhận</p>
-          <p>Số điện thoại</p>
-          <p>Địa chỉ</p>
-          <p>Loai rau</p>
-          <p>Số lượng</p>
-        </div>
-        <div style={{ marginLeft: 10 }}>
-          <p>{giver}</p>
-          <p>{receiver}</p>
-          <p>02258855</p>
-          <a
-            href={`https://www.google.com/maps/search/?api=1&query=${address.lat},${address.long}&query_place_id=${address.place_id}`}
-          >
-            {address.formatted_address}
-            <img src={rightIcon} width={size} height={size} />
-          </a>
+  {
+    return (
+      <React.Fragment>
+        <img src={qrcode} height={qrcodeSize} width={qrcodeSize} />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            marginLeft: 10,
+            width: "max-content",
+          }}
+        >
+          <div className="styling">
+            <p>Người cho </p>
+            <p>Người nhận</p>
+            <p>Số điện thoại</p>
+            <p>Địa chỉ</p>
+            <p>Loai rau</p>
+            <p>Số lượng</p>
+          </div>
+          <div style={{ marginLeft: 10 }}>
+            <p>{giver}</p>
+            <p>{receiver}</p>
+            <p>02258855</p>
+            <a
+              href={`https://www.google.com/maps/search/?api=1&query=${address.lat},${address.long}&query_place_id=${address.place_id}`}
+            >
+              {address.formatted_address}
+              <img src={rightIcon} width={size} height={size} />
+            </a>
 
-          <p>{vegetable}</p>
-          <p>{quantity}</p>
+            <p>{vegetable}</p>
+            <p>{quantity}</p>
+          </div>
         </div>
-      </div>
-    </React.Fragment>
-  );
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
