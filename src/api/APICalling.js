0d6b429923c9cb74a1953874d00bfceb6fc30520
sam <42427441+Go-ixeSam@users.noreply.github.com>
2021-06-03 11:16:12 +0700

    Cập nhật lại Google API key

--- src/api/APICalling.js
+++ src/api/APICalling.js
@@ -13,7 +13,7 @@ const APICalling = {
     const url = "/api/geocode/json";
     //! kiểu put ở dưới là cách để thiết lập đối với những api mà sử dụng query param
     return axios.default.get(
-      `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyA20DZM-KQ30vUa63XoopRiJIzyis_t21I`,
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=SpqyRuAqmzV9p082NJTYtlaZrnmii6g`,
       null
     );
   },
